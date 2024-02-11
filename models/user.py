52c2d7bfdb10108d34403b786aba5484842f7749
Abdellahsyani <syaniabdos@gmail.com>
2024-02-11 18:16:41 +0100

    fix user

--- models/user.py
+++ models/user.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """The User class to craete user instance"""
+
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """Start using the user class"""
+
     email = ""
     password = ""
     first_name = ""
